819fe6015b3bad4af5d005834d1f865531b3940a
Mojtaba sha'bani <msh.golden@gmil.com>
2022-05-12 23:07:48 +0430

    fix issue bitimen

--- exchanges/bitimen.php
+++ exchanges/bitimen.php
@@ -34,48 +34,53 @@ function Bitimen($coin){
         default : $api_url = Telegram("Bitimen: Invalid Input!");
     }
 
-    $response_data = json_decode(file_get_contents($api_url));
+    $orders = @file_get_contents($api_url);
 
-    // echo "<pre>";
-    // print_r($response_data);
-    // echo "</pre>";
+    if($orders !== FALSE){
 
-    $best_seller = MinimumBuyOrderArrayRIAL($response_data->asks)[0];
-    $amount_best_seller = MinimumBuyOrderArrayRIAL($response_data->asks)[1];
+        $response_data = json_decode($orders);
 
-    $best_buyyer = MinimumBuyOrderArrayRIAL($response_data->bids)[0];
-    $amount_best_buyyer = MinimumBuyOrderArrayRIAL($response_data->bids)[1];
+        // echo "<pre>";
+        // print_r($response_data);
+        // echo "</pre>";
 
-    // echo "<br>Bitimen: Best seller: $best_seller , Amount: $amount_best_seller <br>Best buyyer: $best_buyyer , Amount: $amount_best_buyyer";
+        $best_seller = MinimumBuyOrderArrayRIAL($response_data->asks)[0];
+        $amount_best_seller = MinimumBuyOrderArrayRIAL($response_data->asks)[1];
 
-    // echo "<br> Number of Global Exchange: " . sizeof($exchange);
-    $i = sizeof($exchange);
-    $id = GetIndexExchangeByName($exchange_name);
+        $best_buyyer = MinimumBuyOrderArrayRIAL($response_data->bids)[0];
+        $amount_best_buyyer = MinimumBuyOrderArrayRIAL($response_data->bids)[1];
 
-    // echo "<br> GetIndexExchangeByName: " . GetIndexExchangeByName($exchange_name);
+        // echo "<br>Bitimen: Best seller: $best_seller , Amount: $amount_best_seller <br>Best buyyer: $best_buyyer , Amount: $amount_best_buyyer";
 
-    if ($id == NULL){
-        // echo "Not find!";
-        $exchange[$i][0] = $exchange_name;
-        $exchange[$i][1] = $coin;
-        $exchange[$i][2] = intval($best_seller * 10);
-        $exchange[$i][3] = intval($best_buyyer * 10);
-        $exchange[$i][4] = floatval($amount_best_seller);
-        $exchange[$i][5] = floatval($amount_best_buyyer);
-        $exchange[$i][6] = 0; //Flag
-        $exchange[$i][7] = 0; //Gap
-        $exchange[$i][8] = Fee_table($coin);
-    }else{
-        // echo "find!";
-        $exchange[$id][0] = $exchange_name;
-        $exchange[$id][1] = $coin;
-        $exchange[$id][2] = intval($best_seller * 10);
-        $exchange[$id][3] = intval($best_buyyer * 10);
-        $exchange[$id][4] = floatval($amount_best_seller);
-        $exchange[$id][5] = floatval($amount_best_buyyer);
-        $exchange[$id][6] = 0; //Flag
-        $exchange[$id][7] = 0; //Gap
-        $exchange[$id][8] = Fee_table($coin);
+        // echo "<br> Number of Global Exchange: " . sizeof($exchange);
+        $i = sizeof($exchange);
+        $id = GetIndexExchangeByName($exchange_name);
+
+        // echo "<br> GetIndexExchangeByName: " . GetIndexExchangeByName($exchange_name);
+
+        if ($id == NULL){
+            // echo "Not find!";
+            $exchange[$i][0] = $exchange_name;
+            $exchange[$i][1] = $coin;
+            $exchange[$i][2] = intval($best_seller * 10);
+            $exchange[$i][3] = intval($best_buyyer * 10);
+            $exchange[$i][4] = floatval($amount_best_seller);
+            $exchange[$i][5] = floatval($amount_best_buyyer);
+            $exchange[$i][6] = 0; //Flag
+            $exchange[$i][7] = 0; //Gap
+            $exchange[$i][8] = Fee_table($coin);
+        }else{
+            // echo "find!";
+            $exchange[$id][0] = $exchange_name;
+            $exchange[$id][1] = $coin;
+            $exchange[$id][2] = intval($best_seller * 10);
+            $exchange[$id][3] = intval($best_buyyer * 10);
+            $exchange[$id][4] = floatval($amount_best_seller);
+            $exchange[$id][5] = floatval($amount_best_buyyer);
+            $exchange[$id][6] = 0; //Flag
+            $exchange[$id][7] = 0; //Gap
+            $exchange[$id][8] = Fee_table($coin);
+        }
     }
 }
 
